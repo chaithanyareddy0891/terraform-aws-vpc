b8dd3bd0d33c3e25e5226aece5b6cf49da151655
chaithanyareddy <chaithanya.reddy0891@gmail.com>
2026-05-24 07:11:23 +0530

    adding vpc module

---
+++ outputs.tf
@@ -0,0 +1,39 @@
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
+output "igw_id" {
+    value = aws_internet_gateway.main.id
+}
+
+output "az_info" {
+  value = data.aws_availability_zones.available
+}
+
+output "default_vpc_info" {
+    value = data.aws_vpc.default
+}
+
+output "default_vpc_id" {
+  value = data.aws_vpc.default.id
+}
+
+output "default_route_table_id" {
+  value = data.aws_route_table.default.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
+
+output "database_subnet_group_name" {
+  value = aws_db_subnet_group.default.name
+}
